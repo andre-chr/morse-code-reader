90aaf28cf169c64d02e52918bb1385020c80feaa
Ryan Hechenberger <rhec3@student.monash.edu>
2017-05-15 12:48:51 +1000

    updated firebase source code to es6 standard class

--- server/firebase_admin.js
+++ server/firebase_admin.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const admin = require('firebase-admin');
 
 // Fetch the service account key JSON file contents
@@ -9,31 +11,35 @@ admin.initializeApp({
   databaseURL: "https://project1-9aa9d.firebaseio.com"  // IMPORTANT: repalce the url with yours 
 });
 
-function Firebase() {
-	// As an admin, the app has access to read and write all data, regardless of Security Rules
-	this.db = admin.database();
-	this.ref = db.ref("motionSensorData"); // channel name
-	this.resetDb();		/resets database
-}
+class Firebase
+{
+	constructor() {
+		// As an admin, the app has access to read and write all data, regardless of Security Rules
+		this.db = admin.database();
+		this.ref = this.db.ref("motionSensorData"); // channel name
+		this.resetDb();		//resets database
+	}
 
-Firebase.prototype.resetDb = function() {
-	this.ref.set({
-		motionCount: 0,
-		message: ''
-	});
-}
+	resetDb() {
+		this.ref.set({
+			motionCount: 0,
+			message: ''
+		});
+	}
 
-Firebase.prototype.updateCount = function(count) {
-	this.ref.update({
-		motionCount: count
-	});
+	updateCount(count) {
+		this.ref.update({
+			motionCount: count
+		});
+	}
 
-Firebase.prototype.updateMessage = function(msg) {
-	this.ref.update({
-		message: msg
-	}).then(
-		console.log('Message updated!');
-	);
+	updateMessage(msg) {
+		this.ref.update({
+			message: msg
+		}).then(() => {
+			console.log('Message updated!');
+		});
+	}
 }
 
 module.exports = Firebase;
