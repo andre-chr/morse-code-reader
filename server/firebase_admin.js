114f95d214ad7b9c5cf5e532814ae8256bece211
achr18 <achr18@student.monash.edu>
2017-05-15 23:40:17 +1000

    added morse code decoding functionality

--- server/firebase_admin.js
+++ server/firebase_admin.js
@@ -6,14 +6,14 @@ const serviceAccount = require('./serviceAccountKey.json');
 // Initialize the app with a service account, granting admin privileges
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://project1-9aa9d.firebaseio.com"  // IMPORTANT: repalce the url with yours 
+  databaseURL: "https://morse-code-decoder-213b9.firebaseio.com/"
 });
 
 function Firebase() {
 	// As an admin, the app has access to read and write all data, regardless of Security Rules
 	this.db = admin.database();
-	this.ref = db.ref("motionSensorData"); // channel name
-	this.resetDb();		/resets database
+	this.ref = this.db.ref("motionSensorData"); // channel name
+	this.resetDb();		//resets database
 }
 
 Firebase.prototype.resetDb = function() {
@@ -27,13 +27,12 @@ Firebase.prototype.updateCount = function(count) {
 	this.ref.update({
 		motionCount: count
 	});
+}
 
 Firebase.prototype.updateMessage = function(msg) {
 	this.ref.update({
 		message: msg
-	}).then(
-		console.log('Message updated!');
-	);
+	});
 }
 
 module.exports = Firebase;
--- server/server.js
+++ server/server.js
@@ -2,15 +2,13 @@
 
 //import libraries
 var fs = require('fs'), 
-	http = require('http'),
-    socketio = require('socket.io');
+	http = require('http');
 
-// load config file
-const config = require('./config.json');
-// load Board class
-const Board = require('./board.js');
-const Simulator = require('./simulator.js');
-const Firebase = require('./firebase_admin.js');
+
+const config = require('./config.json');			//import config file
+const Board = require('./board.js');				//import board file
+const Simulator = require('./simulator.js');		//import simulator
+const Firebase = require('./firebase_admin.js');	//import firebase file
 
 var server=http.createServer(function(req, res) {
 	res.writeHead(200, { 'Content-type': 'text/html'});
@@ -19,17 +17,56 @@ var server=http.createServer(function(req, res) {
 		console.log('Listening at: http://localhost:8080');
  });
 
+var firebase = new Firebase();	//construct firebase database
+
+var data = {
+	motionCount: 0,
+	message: ''
+}
+var code = '';
+
+function flushLetter() {
+	var c = config["morse-decoder"][code];	//lookup dictionary of morse codes
+	if (c) {
+		data.message += c;
+		firebase.updateMessage(data.message);	//update message to database
+	}
+	code = '';		//reset code
+}
+
+function newWord() {
+	data.message += ' ';
+}
+
+var letterInterval, wordInterval;
+
 //var board = new Board(); // connect to the board
 var board = new Simulator("Hello world!"); // connect to the simulator
 
 board.on('open', () => {
     console.log('board connected');
 });
+
+//triggers when motion starts
 board.on('start', (time) => {
     console.log('motion start; delay: ', + time.toString() + ' ms');
+	data.motionCount++;
+	firebase.updateCount(data.motionCount);		//update count to database
+	
+	//clear timers when motion is detected
+	clearTimeout(letterInterval);
+	clearTimeout(wordInterval);
 });
+
+//triggers when motion ends
 board.on('end', (time) => {
     console.log('motion end; duration: ' + time.toString() + ' ms');
+	
+	code += time >= config['long-mark']*config['time-unit'] ? 'L' : 'S';	//categorize motion as long or short
+	console.log(code);
+	//start timers for letter and word
+	letterInterval = setTimeout(flushLetter, config['letter-gap']*config['time-unit']);
+	wordInterval = setTimeout(newWord, config['word-gap']*config['time-unit']);
 });
 board.open();
 
